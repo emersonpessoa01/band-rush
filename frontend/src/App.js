f9e685afa5faf0a12ff4afe5bcdfa5185f46138b
emersonpessoa01 <eps011108@gmail.com>
2020-09-30 10:08:43 -0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,7 +51,7 @@ export default class App extends Component {
     return (
       <div className="container">
         {/* <Header title="Votação" /> */}
-        <Header>Índice de popularidade de Hollywood</Header>
+        <Header>Índice de popularidade em Hollywood</Header>
         <Candidates previousPercentages={previousPercentages} previousVotes={previousVotes} candidates={candidates} />
       </div>
     );
