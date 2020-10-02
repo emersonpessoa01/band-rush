1503f39b776d964979ea013d8364987d24b6cf92
emersonpessoa01 <eps011108@gmail.com>
2020-10-02 16:56:01 -0300

    Updates

---
+++ precache-manifest.0534e65016a890b5dab4bd14b9820747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f150468ae1ce0bb13f4710c3d23bedde",
+    "url": "/react-family/index.html"
+  },
+  {
+    "revision": "2035391104aa92ed0e00",
+    "url": "/react-family/static/css/main.2223bb8d.chunk.css"
+  },
+  {
+    "revision": "dbd9f5510c01aad1a2f4",
+    "url": "/react-family/static/js/2.6fc78457.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-family/static/js/2.6fc78457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2035391104aa92ed0e00",
+    "url": "/react-family/static/js/main.2963a222.chunk.js"
+  },
+  {
+    "revision": "46093ebe48fbf7e25879",
+    "url": "/react-family/static/js/runtime-main.9e4aa1de.js"
+  }
+]);
