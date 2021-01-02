60668c83d4b1678e6b8da5dae9c34daf0a31ffe6
emersonpessoa01 <eps011108@gmail.com>
2021-01-02 15:50:03 -0300

    updated

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ let candidates = [];
  * Intervals que serão executados
  * a cada x intervalo de tempo
  */
-// let intervalVotes = null;
-// let intervalPopularity = null;
+let intervalVotes = null;
+let intervalPopularity = null;
 
 /**
  * Valores constantes importantes
@@ -195,7 +195,7 @@ app.get('/votes', (_, res) => {
  * Iniciando o servidor
  * na porta 3001
  */
-app.listen(3001);
+app.listen(8080);
 
 /**
  * Execução inicial
